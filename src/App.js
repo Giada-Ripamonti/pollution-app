9a649c076ae3b769e70fceccf78a30f0ab4fc4b8
Giada-Ripamonti <ripamontigiada@gmail.com>
2022-09-09 16:24:05 +0200

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Location from './components/Location';
+import Pollution from './components/Pollution';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>air quality monitor</h1>
+      <Location />
+      <Container>
+        <Row>
+          <Pollution />
+        </Row>
+      </Container>
     </div>
   );
 }
